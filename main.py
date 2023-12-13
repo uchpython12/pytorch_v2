b1d99341de7bb3c81cae8261980f495105b41d4a
uchpython12 <110381986+uchpython12@users.noreply.github.com>
2023-12-13 20:56:53 +0800

    rnn

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import torch
-
+from d2l import torch as d2l
 # 检查GPU是否可用
+gpu_count = torch.device("mps")
+print(gpu_count)
+d2l.try_gpu()
 if torch.cuda.is_available():
     # 获取GPU数量
-    gpu_count = torch.cuda.device_count()
+    # gpu_count = torch.cuda.device_count()
+    gpu_count = torch.device("mps")
     print(f"发现 {gpu_count} 个可用的GPU")
 
     # 遍历每个GPU并打印信息
@@ -12,3 +16,5 @@ if torch.cuda.is_available():
         print(f"GPU {i}: {gpu_name}")
 else:
     print("未找到可用的GPU")
+
+
